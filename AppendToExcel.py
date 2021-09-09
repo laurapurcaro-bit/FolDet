043d4fe6c46820c69bde52c157cfaa7bfbab5191
Laura P <77932305+laurapurcaro-bit@users.noreply.github.com>
2021-09-09 17:52:45 +0200

    Add files via upload

---
+++ AppendToExcel.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+
+# author          :     Laura Purcaro
+# title           :     AppendToExcel.py
+# description     :     Helper function for Evaluation metrics.py
+# date            :     02.05.2021
+# version         :     1.0
+# python_version  :     3.8.8
+
+
+"""
+
+# Libraries
+from openpyxl import load_workbook
+import os
+import pandas as pd
+
+###########################################################################
+# AppendToExcel: 
+# The algorithm contains append_df_to_excel function to append new dataframe 
+# to existing excel file
+
+###########################################################################
+
+def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
+                       truncate_sheet=False, 
+                       **to_excel_kwargs):
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+
+    Usage examples:
+
+    >>> append_df_to_excel('d:/temp/test.xlsx', df)
+
+    >>> append_df_to_excel('d:/temp/test.xlsx', df, header=None, index=False)
+
+    >>> append_df_to_excel('d:/temp/test.xlsx', df, sheet_name='Sheet2',
+                           index=False)
+
+    >>> append_df_to_excel('d:/temp/test.xlsx', df, sheet_name='Sheet2', 
+                           index=False, startrow=25)
+
+    """
+    # Excel file doesn't exist - saving and exiting
+    if not os.path.isfile(filename):
+        df.to_excel(
+            filename,
+            sheet_name=sheet_name, 
+            startrow=startrow if startrow is not None else 0, 
+            **to_excel_kwargs)
+        return
+    
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl', mode='a')
+
+    # try to open an existing workbook
+    writer.book = load_workbook(filename)
+    
+    # get the last row in the existing Excel sheet
+    # if it was not specified explicitly
+    if startrow is None and sheet_name in writer.book.sheetnames:
+        startrow = writer.book[sheet_name].max_row
+
+    # truncate sheet
+    if truncate_sheet and sheet_name in writer.book.sheetnames:
+        # index of [sheet_name] sheet
+        idx = writer.book.sheetnames.index(sheet_name)
+        # remove [sheet_name]
+        writer.book.remove(writer.book.worksheets[idx])
+        # create an empty sheet [sheet_name] using old index
+        writer.book.create_sheet(sheet_name, idx)
+    
+    # copy existing sheets
+    writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+
+    if startrow is None:
+        startrow = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
+
+    # save the workbook
+    writer.save()
+    
+# Make the module callable 
+if __name__ == "__main__":
+    
+    append_df_to_excel()
